53879c60990d7b4e3a47611ec279973f3b9eadd7
SefaKahramannn <sefakahraman0001@gmail.com>
2025-03-17 15:32:06 +0300

    TraceViewer Done

---
+++ src/test/java/TraceViewer.java
@@ -0,0 +1,48 @@
+import com.microsoft.playwright.*;
+
+import java.awt.*;
+import java.nio.file.Paths;
+
+public class TraceViewer {
+    public static void main(String[] args) {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) dimension.getWidth();
+        int height = (int) dimension.getHeight();
+
+        BrowserContext context=browser.newContext();
+
+        // Start tracing before creating / navigate a page
+        context.tracing().start(new Tracing.StartOptions()
+                .setScreenshots(true)
+                .setSnapshots(true)
+                .setSources(true)
+        );
+
+        Page page = context.newPage();
+        page.setViewportSize(width,height);
+
+        page.navigate("https://demoqa.com/text-box");
+
+        page.getByPlaceholder("Full Name").fill("Jonah");
+        page.getByPlaceholder("name@example.com").fill("test@test.com");
+        page.getByPlaceholder("Current Address").fill("test current address");
+        page.locator("[id='permanentAddress']").fill("test permanent address");
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        // Stop tracing and export it into a zip archive
+        context.tracing().stop(new Tracing.StopOptions()
+                .setPath(Paths.get("trace.zip"))
+        );
+
+        context.close();
+        page.close();
+        playwright.close();
+    }
+}
