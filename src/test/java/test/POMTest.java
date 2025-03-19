54a72727f04cc1d2ed3b2e42e4a1b0897c65b6bb
SefaKahramannn <sefakahraman0001@gmail.com>
2025-03-19 16:37:47 +0300

    Allure Report added

--- src/test/java/test/POMTest.java
+++ src/test/java/test/POMTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import io.qameta.allure.*;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -8,13 +9,21 @@ import static utilities.Hooks.page;
 @Listeners(utilities.Hooks.class)
 public class POMTest {
 
+    @Epic("User Interface Tests")
+    @Feature("Login Feature")
+    @Story("Successful Login")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("Test for successful login with valid credentials")
     @Test
     void shouldNavigate(){
-        page.navigate("https://demoqa.com/text-box");
-
+        Allure.step("Navigate to domain",() ->{
+            page.navigate("https://demoqa.com/text-box");
+        });
         page.getByPlaceholder("Full Name").fill("Jonah");
         page.getByPlaceholder("name@example.com").fill("test@test.com");
         page.getByPlaceholder("Current Address").fill("test current address");
-        page.locator("[id='permanentAddres']").fill("test permanent address"); // locator yanlış traceViewer ın çalıştığını görmek için yapıldı
+        Allure.step("Invalid Locator",()-> {
+            page.locator("[id='permanentAddres']").fill("test permanent address"); // locator yanlış traceViewer ın çalıştığını görmek için yapıldı
+        });
     }
 }
